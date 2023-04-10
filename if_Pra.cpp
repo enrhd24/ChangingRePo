51d924c6de874c5692679afb59aecd4a7d3c1702
enrhd25_ <128342082+enrhd24@users.noreply.github.com>
2023-04-10 15:56:00 +0900

    if_statement pra 2

--- if_Pra.cpp
+++ if_Pra.cpp
@@ -1,22 +1,13 @@
 int solution(vector<int> box, int n) {
-    int answer = 0;
-    
-    int num1 = 7;
-    int num2 = 12;
-    int num3 = 3;
-    
-    int max = 0;
-    int min = 0;
-    
-    // 1. max
-    if (max < num1){max = num1;}else{max = max;}
-    if (max < num2){max = num2;}else{max = max;}
-    if (max < num3){max = num3;}else{max = max;}
-    
-    // 2. min
-    min = num1;
-    if (min < num2){min = min;}else{min = num2;}
-    if (min < num3){min = min;}else{min = num3;}
-    
-    return min;
+  int num1 = 7;
+  int num2 = 12;
+  int num3 = 3;
+  bool isEven = false;
+
+  if(num1 % 2 == 0){isEven = true;}else{isEven = false;}
+  if(num2 % 2 == 0){isEven = true;}else{isEven = false;}
+  if(num3 % 2 == 0){isEven = true;}else{isEven = false;}
+
+  int sum =  num1 + num2 + num3;
+  int mul = num1 * num2 * num3;
 }
