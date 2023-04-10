28a48cd42ccf7ee88734bc567c2fb2e35ec40079
enrhd25_ <128342082+enrhd24@users.noreply.github.com>
2023-04-10 15:03:53 +0900

    협업을 위한 LED_ON

--- blink.ino
+++ blink.ino
@@ -7,22 +7,25 @@ void setup() {
   pinMode(LED_PIN_1, OUTPUT);
   pinMode(LED_PIN_2, OUTPUT);
   pinMode(LED_PIN_3, OUTPUT);
+  digitalWrite(LED_PIN_1, HIGH);
+  digitalWrite(LED_PIN_2, HIGH);
+  digitalWrite(LED_PIN_3, HIGH);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_PIN_1, HIGH);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_PIN_1, LOW);  // turn the LED on (HIGH is the voltage level)
   delay(500);                      // wait for a second
-  digitalWrite(LED_PIN_1, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(LED_PIN_1, HIGH);   // turn the LED off by making the voltage LOW
   delay(500);                      // wait for a second
 
-   digitalWrite(LED_PIN_2, HIGH);  // turn the LED on (HIGH is the voltage level)
+   digitalWrite(LED_PIN_2, LOW);  // turn the LED on (HIGH is the voltage level)
   delay(500);                      // wait for a second
-  digitalWrite(LED_PIN_2, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(LED_PIN_2, HIGH);   // turn the LED off by making the voltage LOW
   delay(500); 
 
-   digitalWrite(LED_PIN_3, HIGH);  // turn the LED on (HIGH is the voltage level)
+   digitalWrite(LED_PIN_3, LOW);  // turn the LED on (HIGH is the voltage level)
   delay(500);                      // wait for a second
-  digitalWrite(LED_PIN_3, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(LED_PIN_3, HIGH);   // turn the LED off by making the voltage LOW
   delay(500); 
 }
