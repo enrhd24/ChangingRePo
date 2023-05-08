06afbeda85e86a84be337d61fa225be523cbe689
enrhd24 <enrhd567@gmail.com>
2023-05-08 12:35:34 +0900

    Dart finished

--- Dart/Classes.dart
+++ Dart/Classes.dart
@@ -1,9 +1,48 @@
+ // Abstract Classes
+ abstract class Human{
+  void walk();
+ }
+ 
+ //enum
+  enum Team {red, blue}
+  enum XPLEvel {beginner, medium, pro}
 
-class Player {
+  // Inheritance
+  class Inheritance {
+    final String name;
+    Inheritance(this.name);
+
+    void sayHello(){print("Hi my name is $name");}
+  }
+  // 상속받는 해당 클래스
+  class Player extends Inheritance{
+    final Team team;
+
+    Player({
+      required this.team,
+      required String name
+    }) : super(name); //부모클래스와 상호작용할 수 있도록 하는 것.
+  }
+  @override
+  void sayHello(){
+    syper.sayHello();
+    print('and I play for ${team}');
+  }
+
+// extends Human ->  abstract class
+class Player extends Human{
   late final String name;
-  late int xp;
-  String team;
+  XPLevel xp; // enum
+  Team team; //enum
   int age;
+
+  // 추상클래스의 해당 함수의 형태를 변경해도 된다.
+  class Coach extends Human{
+    void walk(){
+      print("변경된 print 입니닷");
+    }
+  }
+
 
   //Constructors
   Player(String name, int xp, String team, int age){
@@ -14,7 +53,7 @@ class Player {
   Player({
     this.name, this.xp, 
     this.team, this.age
-    });
+    })
 
   // Named constructors
   Player.createBluePlayer({required String name, required age}) 
@@ -29,7 +68,12 @@ class Player {
   : 
   name = playerJson['name'],
   xp = playerJson['xp'],
-  team - playerJson['team'],
+  team = playerJson['team'],
+
+  //Abstract Classws
+  void walk(){
+    peint("i am walking");
+  }
   
   void sayHello(){
     print("Hi name is $name")
@@ -46,6 +90,13 @@ void main(){
     age : 21,
   );
   print(player.name);
+
+  // Cascade Notation
+  var player = new Player(name : "YeJin",xp : 2300,team : 'blue',age : 21,)
+  ..name = 'las'
+  ..XPLEvel = xp.medium //enum
+  ..team = Team.blue //enum
+  ..sayHello();
 
   var player.name = 'lalala';
   print(plater.name);
@@ -65,4 +116,9 @@ void main(){
   apiData.forEach((playerJson){
     var player = player.fromJson(playerJson);
     player.sayHello();})
+
+    //Inheritance
+    var player = Player(team: Team.red, name:'YeJin');
+    // super 쓴 경우
+
   }
