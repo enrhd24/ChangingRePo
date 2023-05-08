bd558823acbfc07a7f5e868b9a811a6b6e535657
enrhd24 <enrhd567@gmail.com>
2023-05-08 09:33:01 +0900

    Dart Classed Constructor added

--- Dart/Classes.dart
+++ Dart/Classes.dart
@@ -1,20 +1,68 @@
 
 class Player {
-  String name = 'nice';
-  int xp = 1500;
+  late final String name;
+  late int xp;
+  String team;
+  int age;
 
+  //Constructors
+  Player(String name, int xp, String team, int age){
+    this.name = name; this.xp = xp;
+    this.team = team; this.age = age;
+  }
+  // {} -> NAmed Constructor Parameters
+  Player({
+    this.name, this.xp, 
+    this.team, this.age
+    });
+
+  // Named constructors
+  Player.createBluePlayer({required String name, required age}) 
+    : // : -> 변수 초기화하는 것
+    this.age = age,
+    this.name = name,
+    this.team = 'blue',
+    this.xp = 0;
+
+  //Recap
+  Player.fromJson(Map<String, dynamic> playerJson)
+  : 
+  name = playerJson['name'],
+  xp = playerJson['xp'],
+  team - playerJson['team'],
+  
   void sayHello(){
     print("Hi name is $name")
   }
+
 }
 
 void main(){
-  var player = new Player();
-  print(plater.name);
+  // Named Constructor Parameters
+  var player = new Player(
+    name : "YeJin",
+    xp : 2300,
+    team : 'blue',
+    age : 21,
+  );
+  print(player.name);
 
   var player.name = 'lalala';
   print(plater.name);
 
-  var.player = Player("nice", 1500);
-  player.sayHello();
+  var.player1 = Player("nice", 1500);
+  var.player2 = Player("lynn", 1500);
+  player1.sayHello();
+  player2.sayHello();
+
+  // Named Constructors [using example]
+  var redPlayer = Player.createBluePlayer(
+    name : "YeJin",
+    age : 12,
+    )
+
+  // Recap aptData -> 데이터 입력
+  apiData.forEach((playerJson){
+    var player = player.fromJson(playerJson);
+    player.sayHello();})
   }
